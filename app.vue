12ec133c205c3436005834b389d3d045dda520fc
Vinícius Marcili <vinipetter@gmail.com>
2023-01-29 09:20:02 -0300

    Finish base

--- app.vue
+++ app.vue
@@ -1,9 +1,25 @@
-<script setup>
+<script lang="ts" setup>
+import { useI18n } from '#imports'
+const { t, locale, setLocale } = useI18n()
 const runtimeConfig = useRuntimeConfig()
+onBeforeMount(() => {
+  if (navigator?.language?.includes('pt')) {
+    document.documentElement.setAttribute('lang', 'pt-BR')
+    setLocale('pt')
+  } else if (navigator?.language?.includes('es')) {
+    document.documentElement.setAttribute('lang', 'es-ES')
+    setLocale('es')
+  } else {
+    document.documentElement.setAttribute('lang', 'en-US')
+    setLocale('en')
+  }
+})
 </script>
 <template>
   <div>
     <p>Private config: {{ runtimeConfig.public.name }}</p>
+    <p>{{ t('language') }}</p>
+    <p>{{ locale }}</p>
   </div>
 </template>
 <style lang="scss"></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,103 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
+const langs = ['en', 'pt', 'es']
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['~/src/assets/styles/global.scss'],
+  modules: ['@nuxtjs/i18n'],
+  i18n: {
+    locales: langs.map((l) => {
+      return {
+        code: l,
+        iso: l,
+        name: l,
+        files: [`${l}.json`]
+      }
+    }),
+    lazy: true,
+    langDir: 'locales',
+    defaultLocale: 'en',
+    vueI18n: {
+      fallbackLocale: 'en'
+    }
+  },
   app: {
     head: {
       title: 'Mediakit',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: 'Mediakit Squid' }
+        { hid: 'description', name: 'description', content: 'Mediakit Squid' },
+        { name: 'application-name', content: 'Mediakit Squid' },
+        { name: 'apple-mobile-web-app-title', content: 'Mediakit Squid' },
+        { name: 'mobile-web-app-capable', content: 'yes' },
+        { name: 'msapplication-TileColor', content: '#9dd6e8' },
+        { name: 'theme-color', content: '#9dd6e8' },
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:site', content: '@squidinfluencers' },
+        { name: 'twitter:creator', content: '@squidinfluencers' },
+        { name: 'twitter:title', content: 'Mediakit Squid' },
+        { name: 'twitter:description', content: 'Mediakit Squid' },
+        { name: 'twitter:image', content: '/img/favicon.png' },
+        { name: 'twitter:image:alt', content: 'Mediakit Squid' },
+        { name: 'og:title', content: 'Mediakit Squid' },
+        { name: 'og:description', content: 'Mediakit Squid' },
+        { name: 'og:image', content: '/img/favicon.png' },
+        { name: 'og:image:alt', content: 'Mediakit Squid' },
+        { name: 'og:image:width', content: '512' },
+        { name: 'og:image:height', content: '512' },
+        { name: 'og:image:type', content: 'image/png' },
+        { name: 'og:site_name', content: 'Mediakit Squid' },
+        { name: 'og:type', content: 'website' },
+        { name: 'og:url', content: 'https://mediakit.squidit.com.br' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/img/favicon.ico' },
+        {
+          rel: 'shortcut icon',
+          type: 'image/x-icon',
+          href: '/img/favicon.ico'
+        },
+        {
+          rel: 'icon',
+          sizes: '16x16',
+          type: 'image/png',
+          href: '/img/favicon.png'
+        },
+        {
+          rel: 'icon',
+          sizes: '32x32',
+          type: 'image/png',
+          href: '/img/favicon.png'
+        },
+        {
+          rel: 'icon',
+          sizes: '192x192',
+          type: 'image/png',
+          href: '/img/favicon.png'
+        },
+        {
+          rel: 'icon',
+          sizes: '194x194',
+          type: 'image/png',
+          href: '/img/favicon.png'
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          type: 'image/png',
+          href: '/img/favicon.png'
+        },
+        {
+          rel: 'mask-icon',
+          href: '/img/favicon.png',
+          color: '#9dd6e8'
+        }
       ],
       bodyAttrs: {
         class: 'scrollbar light'
-      }
+      },
+      htmlAttrs: {}
     }
   },
   runtimeConfig: {
@@ -35,7 +119,7 @@ export default defineNuxtConfig({
         heimdall: process.env.HEIMDALL_API_URL,
         x23: process.env.X23_API_URL
       },
-      langs: ['en', 'pt', 'es']
+      langs
     }
   }
 })
